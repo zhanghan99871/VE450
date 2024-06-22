69c81431279c10d9afde740aafad0e11552cd831
kalium <kalium222@outlook.com>
2024-06-22 22:45:42 +0800

    feat: load the rgb from png and luminance from txt

--- RawImage.py
+++ RawImage.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2 as cv
-from PIL import Image
 
 
 class RawImage:
@@ -8,11 +7,24 @@ class RawImage:
         self.luminance = np.zeros((x, y, 1))
         self.rgb = np.zeros((x, y, 3))
 
-    def loadRGB(png_file):
-        # TODO:
-        pass
+    def loadRGB(self, png_file):
+        img = cv.imread(png_file)
+        self.rgb = np.array(cv.cvtColor(img, cv.COLOR_BGR2RGB))
 
-    def loadLuminance(txt_file):
-        # TODO:
-        pass
+    def loadLuminance(self, txt_file):
+        with open(txt_file, 'r') as file:
+            lines = file.readlines()
+            # flag for reading the content below the "X Y value" line
+            start_reading = False
+            for line in lines:
+                if start_reading:
+                    parts = line.strip().split()
+                    # TODO: add the correct number according to
+                    # the txt file's begining lines
+                    x = float(parts[0]) + 15
+                    y = float(parts[1]) + 7
+                    value = float(parts[2])
+                    self.luminance[int(y / 0.02)][int(x / 0.02)][0] = value
+                if 'X' in line:
+                    start_reading = True
 
