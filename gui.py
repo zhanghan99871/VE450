1e527881f1343bea7721b0a2aaa0a87ebb004ab9
zhanghan99871 <zhanghan07@sjtu.edu.cn>
2024-07-12 10:32:18 +0800

    data retrival implemented

--- gui.py
+++ gui.py
@@ -3,8 +3,13 @@ import tkinter
 from PIL import Image, ImageTk
 from tkinter import filedialog
 import numpy as np
+import colorsys
 MAX_WIDTH = 1500
 MAX_HEIGHT = 800
+color_space = {}
+color_space["RGB"] = 0
+color_space["HSL"] = 1
+
 
 class GUI:
     def __init__(self, tk):
@@ -14,6 +19,7 @@ class GUI:
         self.y = 0
         self.tk = tk
         self.menu = Menu(self.tk)
+        self.color_mode = color_space["RGB"]
         # self.text_widget = Text(self.tk, height=5, width=25)
         # self.text_widget.pack(pady=10)
         self.w_width = self.tk.winfo_width()
@@ -41,7 +47,7 @@ class GUI:
         toolmenu.add_command(label="Luminance adjustment", command=self.luminance_adjust)
         toolmenu.add_command(label="Gamut clipping", command=self.gamut_clip)
         toolmenu.add_command(label="Rescale to original size", command=self.rescale)
-        toolmenu.add_command(label="Color space", command=self.change_color_space)
+        toolmenu.add_command(label="Change color space", command=self.change_color_space)
         toolmenu.add_command(label="Get value", command=self.get_value)
 
         helpmenu = Menu(self.menu)
@@ -56,10 +62,36 @@ class GUI:
         pass
 
     def change_color_space(self):
-        pass
+        self.color_mode = (self.color_mode + 1) % 2
+        print(self.color_mode)
 
     def get_value(self):
-        pass
+        self.create_sub_window()
+
+    def create_sub_window(self): # sub window to get value
+        self.sub_window = Toplevel(self.tk)
+        self.sub_window.title("Sub Window")
+        x_label = Label(self.sub_window, text="x: ")
+        x_label.grid(row=0, column=0)
+        self.x_entry = Entry(self.sub_window, width=30)
+        self.x_entry.grid(row=0, column=1)
+        y_label = Label(self.sub_window, text="y: ")
+        y_label.grid(row=0, column=2)
+        self.y_entry = Entry(self.sub_window, width=30)
+        self.y_entry.grid(row=0, column=3)
+        result_label = Label(self.sub_window, text="value is:({}, {}, {})".format(0, 0, 0))
+        result_label.grid(row=3, column=2)
+        def submit():
+            # Retrieve the data from the entry widget
+            if self.image:
+                x_input = self.x_entry.get()
+                y_input = self.y_entry.get()
+                rgb = self.get_value_from_color_space(x_input, y_input)
+                # Display or use the data (here we update a label as an example)
+                result_label.config(text="value is:({}, {}, {})".format(rgb[0], rgb[1], rgb[2]))
+        button_to_get = Button(self.sub_window, text="Submit", command=submit)
+        button_to_get.grid(row=1, column=2)
+
 
     def rescale(self):
         if self.image:
@@ -121,7 +153,7 @@ class GUI:
 
     def display_pixel(self):
         if self.image:
-            value = np.array(self.image)[int(self.y * self.h_ratio)][int(self.x * self.w_ratio)]
+            value = self.get_value_from_color_space(self.x * self.w_ratio, self.y * self.h_ratio)
             self.canvas.itemconfig(self.text_id,
                                    text="x={}, y={}, value=({}, {}, {})".format(int(self.x * self.w_ratio),
                                                                                 int(self.y * self.h_ratio), value[0],
@@ -165,7 +197,22 @@ class GUI:
         self.display_pixel()
 
 
+    def get_value_from_color_space(self, x, y):
+        image = np.array(self.image)
+        if self.color_mode == color_space["RGB"]:
+            return image[int(y)][int(x)]
+        elif self.color_mode == color_space["HSL"]:
+            return self.rgb_to_hls(image[int(y)][int(x)])
+        else:
+            return [0, 0, 0]
 
+    def rgb_to_hls(self, rgb):
+        r_scaled, g_scaled, b_scaled = rgb[0] / 255.0, rgb[1] / 255.0, rgb[2] / 255.0
+
+        # Convert the RGB values to HSL values
+        h, l, s = colorsys.rgb_to_hls(r_scaled, g_scaled, b_scaled)
+
+        return h, l, s
 
 def main():
     root = Tk()
--- test.py
+++ test.py
@@ -1,15 +1,28 @@
 import tkinter as tk
 
-def resize_window():
-    root.geometry("600x400")
+def submit():
+    # Retrieve the data from the entry widget
+    user_input = entry.get()
+    # Optional: Clear the entry widget after getting the input
+    entry.delete(0, tk.END)
+    # Display or use the data (here we update a label as an example)
+    result_label.config(text="You entered: " + user_input)
 
+# Create the main window
 root = tk.Tk()
-root.geometry("400x300")  # Set initial size
-root.resizable(True, True)  # Allow resizing in both directions
-root.minsize(200, 150)  # Set minimum size
-root.maxsize(800, 600)  # Set maximum size
+root.title("User Input Example")
 
-button = tk.Button(root, text="Resize Window", command=resize_window)
-button.pack(pady=20)
+# Create an Entry widget
+entry = tk.Entry(root, width=50)
+entry.pack(pady=10)
 
+# Create a Button to submit the input
+submit_button = tk.Button(root, text="Submit", command=submit)
+submit_button.pack(pady=5)
+
+# Label to display the results
+result_label = tk.Label(root, text="")
+result_label.pack(pady=10)
+
+# Start the main event loop
 root.mainloop()
